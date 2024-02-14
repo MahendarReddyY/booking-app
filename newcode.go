3c6693c6cc11993c19e6aa325258543f8cd2bf33
MahendarReddyY <147524866+MahendarReddyY@users.noreply.github.com>
2024-02-14 14:40:06 -0600

    Update newcode.go

--- newcode.go
+++ newcode.go
@@ -5,5 +5,7 @@ import "fmt"
 //var firstNames []string
 
 func event2() {
+	fmt.Println("These candidates are completed their bookings:")
 	fmt.Println("list of first names:")
+	fmt.Println("The above list of names are attending the event")
 }
